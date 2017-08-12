6a4b1b5284b60a72307b1d55bfe9370f2e20f419
Laudivan Freire de Almeida <eu@laudivan.info>
2017-08-11 23:37:08 -0300

    Inserindo pontos no mapa e preparando as funções para filtro: swapShowTur (), swapShowArt () e swapShowCult (). Basta usá-las sem argumento.

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -1,17 +1,29 @@
+//TODO verificar porque não está gravando localStorage
+
 var geojsonbase = 'https://raw.githubusercontent.com/bigeyessolution/SmartChico/master/caus/data/geojson.json';
+var lastPosition = [-9.41192,-40.50267];
+var localGeoJSON = false;
+
+var markersClusters = false;
+var showTur = true;
+var showCult = true;
+var showArt = true;
+var map = false;
 
 $(document).on("mobileinit", function () {
     $.mobile.loader.prototype.options.text = "loading";
     $.mobile.loader.prototype.options.textVisible = false;
     $.mobile.loader.prototype.options.theme = "a";
     $.mobile.loader.prototype.options.html = '<i class="fa fa-pulse fa-spin fa-5x fa-fw margin-bottom"></i>';
+
+    initLocalBase();
 });
 
 $(document).on("pagecontainerbeforechange", function (event, ui) {
     //var toPage = $(ui.toPage).attr('id');
     //var prevPage = $(ui.prevPage).attr('id');
 
-    //if (prevPage === 'mapa') {
+    //if (prevPage === 'mapa') {Notgeofeaturese that 'img-src' was not explicitly set
     //}
 });
 
@@ -19,24 +31,38 @@ $(document).on("pagecontainerchange", function (event, ui) {
   var toPage = $(ui.toPage).attr('id');
   var prevPage = $(ui.prevPage).attr('id');
 
-console.log(toPage)
-
   if (toPage === 'mapa')
       criarMapa();
+      getGeoJSON();
   if (prevPage === 'mapa')
     destruirMapa();
 });
 
+function centerToUser () {
+  //TODO Centralizar mapa na posição do usuário
+}
+
+function swapShowTur () { showTur = !showTur; setGeoJSONtoMap(); }
+
+function swapShowArt () { showArt = !showArt; setGeoJSONtoMap(); }
+
+function swapShowCult () {showCult = !showCult; setGeoJSONtoMap(); }
+
+/**
+ * Cria o mapa em #mapa .mapView
+ * @author Laudivan F Almeida <eu@laudivan.info>
+ * @return {[type]} [description]
+ */
 function criarMapa() {
   $('#mapa .mapView').append('<div id="mapLocal">');
 
 	var localLatLng = L.latLng(-23.5928401,-46.6488079);
 
-	var map = L.map('mapLocal', {
+	map = L.map('mapLocal', {
 		zoom: 16,
 		center: localLatLng,
 		zoomControl: false
-	}).setView([-23.5928401,-46.6488079],16);
+	}).setView([-9.41192,-40.50267],16);
 
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
@@ -44,10 +70,19 @@ function criarMapa() {
     id: 'rogeriobastos.08164jab',
     accessToken: 'pk.eyJ1Ijoicm9nZXJpb2Jhc3RvcyIsImEiOiJjaW9vZjBka2UwMDVsdHNrbTc5aDRwMW9hIn0.E8itic_IW42gcgLO12oLJw'
   }).addTo(map);
+
+  markersClusters = L.markerClusterGroup();
+  map.addLayer(markersClusters);
 }
 
+/**
+ * Limpa o mapa
+ * @author Laudivan F Almeida <eu@laudivan.info>
+ * @return {[type]} [description]
+ */
 function destruirMapa() {
     $('#mapa .mapView').empty();
+    map = false;
 }
 
 $("#mpanel").trigger("updatelayout");
@@ -65,45 +100,96 @@ function hideLoading() {
     $.mobile.loading("hide");
 }
 
+function prepareMarkers () {
+  var turMarkers = L.geoJson(localGeoJSON.turismo, {
+    pointToLayer: function(feature, latlng) {
+      var marker = L.marker(latlng);
+      marker.bindPopup(feature.properties.popupContent
+      //  , {minHeight: feature.properties.width, minWidth: feature.properties.width}
+      );
+      return marker;
+    }
+  });
+
+
+}
+
 function setGeoJSONtoMap () {
   //@todo SE TIVER ARMAZENADO CARREGAR NO MAPA EM DIFERENTES CAMADAS
+  if(map === false || localGeoJSON === false) return;
+
+  var mergedFeatures = [];
+
+  if (showCult) mergedFeatures = mergedFeatures.concat(localGeoJSON.cultura);
+
+  if (showArt) mergedFeatures = mergedFeatures.concat(localGeoJSON.arte);
+
+  if (showTur) mergedFeatures = mergedFeatures.concat(localGeoJSON.turismo);
+
+  var markers = L.geoJson(mergedFeatures, {
+    pointToLayer: function(feature, latlng) {
+      var marker = L.marker(latlng);
+      marker.bindPopup(feature.properties.popupContent, {minHeight: feature.properties.width, minWidth: feature.properties.width});
+      return marker;
+    }
+  });
+
+  //map.removeLayer(markersClusters);
+  markersClusters.clearLayers();
+
+  //markersClusters = L.markerClusterGroup();
+
+  markersClusters.addLayer(markers);
+  //map.addLayer(markersClusters);
 }
 
 function getGeoJSON () {
-  var release = [];
-  var turismo = [];
-  var cultura = [];
-  var arte = [];
+  var featuresbase = {
+    release: 0,
+    turismo: [],
+    cultura: [],
+    arte: []
+  };
 
-  function _geofilter(key,feature) {
-    switch (feature.properties.category) {
-      case 'turismo':
-
-        break;
-      case 'cultura':
-
-        break;
-      case 'arte':
-      default:
-
-    }
-    if (feature.properties.category == "turismo") {
-      feature.properties.popupContent = "" +
+  function _geoWrapper(key,feature) {
+    feature.properties.popupContent =
       (feature.properties.description ? feature.properties.description + '<br>':'') +
       (feature.properties.image ? '<img src="' + feature.properties.image + '">' : '') +
       (feature.properties.sound ? '<iframe src="' + feature.properties.sound + '"></iframe>' : '') +
-      (feature.properties.caption ? '<br><h3>' + feature.properties.caption + '<h3>' : '');
-      geofeatures.push(feature);
+      (feature.properties.caption ? '<br><h3>' + feature.properties.caption + '<h3>' : '')
+    ;
+
+    switch (feature.properties.category) {
+      case 'turismo':
+        featuresbase.turismo.push(feature);
+        break;
+      case 'cultura':
+        featuresbase.cultura.push(feature);
+        break;
+      case 'arte':
+        featuresbase.arte.push(feature);
+        break;
+      default:
     }
   }
 
   function _cacheGeoJSON (data) {
     //@todo LER O CAMPO release DA BASE LOCAL e COMPARAR COM O ONLINE
-    // SE O LOCAL FOR MAIOR OU IGUAL RETURN
+    if(localGeoJSON && localGeoJSON.release > data.release) return true;
 
+    featuresbase.release = data.release;
 
-    setGeoJSONtoMap();
+    $.each (data.features, _geoWrapper);
+
+    localGeoJSON = featuresbase;
+    localStorage.setItem('geojsonbase', JSON.stringify(localGeoJSON));
   }
 
-  $.getJSON(geojsonbase, _cacheGeoJSON).fail(setGeoJSONtoMap);
+  $.getJSON(geojsonbase, _cacheGeoJSON).done(setGeoJSONtoMap).fail(setGeoJSONtoMap);
+}
+
+function initLocalBase () {
+  localGeoJSON = localStorage.getItem('geojsonbase') ?
+    JSON.parse(localStorage.getItem('geojsonbase')) :
+    false;
 }
