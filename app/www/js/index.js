db026b7d668dad8456a1e3d1f2c5ca08d4a97492
Laudivan Freire de Almeida <eu@laudivan.info>
2017-12-20 17:43:41 -0300

    Implementando o acompanhamento da posição do usuário e o sistema de mensagens

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -1,14 +1,22 @@
 //TODO verificar porque não está gravando localStorage
 
 var geojsonbase = 'https://raw.githubusercontent.com/bigeyessolution/SmartChico/master/caus/data/geojson.json';
+var msgsbase = 'https://raw.githubusercontent.com/bigeyessolution/SmartChico/master/caus/data/messages.json';
 var lastPosition = [-9.41192,-40.50267];
 var localGeoJSON = false;
+var localMsgs = false;
+var lastMsgId = false;
 
 var userMarker = false;
 var markersClusters = false;
 var map = false;
 var watchId = false;
+var watchGeoTagsId = false;
+var watchMsgId = false;
+var followUser = false;
+var page = false;
 
+/*
 $(document).on("mobileinit", function () {
     $.mobile.loader.prototype.options.text = "loading";
     $.mobile.loader.prototype.options.textVisible = false;
@@ -17,38 +25,62 @@ $(document).on("mobileinit", function () {
 
     initLocalBase();
 
-    $('.be-btn-location').on('click', setMapToUserLocation);
+    // Localização ativada por padrão
+    // $('.be-btn-location').on('click', setMapToUserLocation);
 
     $('.be-btn-menu').on('click', menuToggle);
 
     $('#fTurismo, #fCultura, #fArte').on('change', setGeoJSONtoMap);
 });
+*/
 
 //Remover
+$.mobile.loader.prototype.options.text = "loading";
+$.mobile.loader.prototype.options.textVisible = false;
+$.mobile.loader.prototype.options.theme = "a";
+$.mobile.loader.prototype.options.html = '<i class="fa fa-pulse fa-spin fa-5x fa-fw margin-bottom"></i>';
 initLocalBase();
-$('.be-btn-location').on('click', setMapToUserLocation);
+$('.be-btn-location').on('click', function(){
+    //setMapToUserLocation();
+    setFollowUserPosition(true);
+    map.setZoom(16);
+});
 $('.be-btn-menu').on('click', menuToggle);
 
 $('#fTurismo, #fCultura, #fArte').on('change', setGeoJSONtoMap);
-//end remover
+
+document.addEventListener("resume", onResume, false);
+document.addEventListener("pause", onPause, false);
+
+watchMsg();
+
+
+$( "#mapa .mapView" ).bind( "scrollstart", function(){ setFollowUserPosition(false); } );
 
 $(document).on("pagecontainerbeforechange", function (event, ui) {
-    //var toPage = $(ui.toPage).attr('id');
-    //var prevPage = $(ui.prevPage).attr('id');
-
-    //if (prevPage === 'mapa') {Notgeofeaturese that 'img-src' was not explicitly set
-    //}
+    var toPage = $(ui.toPage).attr('id');
+    var prevPage = $(ui.prevPage).attr('id');
+               
+    if (prevPage === 'mapa') {
+        stopGeoLocationWatch();
+        stopWatchGeoTag();
+    }
 });
 
 $(document).on("pagecontainerchange", function (event, ui) {
   var toPage = $(ui.toPage).attr('id');
   var prevPage = $(ui.prevPage).attr('id');
-
+  
+  page = toPage;
+               
   $('#menu').removeClass('opened-menu');
 
   if (toPage === 'mapa') {
     criarMapa();
+    setFollowUserPosition(true);
+    initGeoLocationWatch ();
     getGeoJSON();
+    watchGeoTag();
   } else if (prevPage === 'mapa'){
     destruirMapa();
   }
@@ -60,6 +92,8 @@ $(document).on("pagecontainerchange", function (event, ui) {
  * @return {[type]} [description]
  */
 function criarMapa() {
+  if (map) return;
+    
   $('#mapa .mapView').append('<div id="mapLocal">');
 
 	var localLatLng = L.latLng(-23.5928401,-46.6488079);
@@ -74,8 +108,7 @@ function criarMapa() {
     px.y -= (e.popup._container.clientHeight + 200)/2;
     map.panTo( map.unproject(px),{animate: true} );
   });
-
-
+    
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     //attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
     maxZoom: 18,
@@ -85,8 +118,6 @@ function criarMapa() {
 
   markersClusters = L.markerClusterGroup();
   map.addLayer(markersClusters);
-
-  setMapToUserLocation ();
 }
 
 /**
@@ -95,8 +126,11 @@ function criarMapa() {
  * @return {[type]} [description]
  */
 function destruirMapa() {
-    $('#mapa .mapView').empty();
-    map = false;
+    if (map !== false) {
+        $('#mapa .mapView').empty();
+        map = false;
+        userMarker = false;
+    }
 }
 
 $("#mpanel").trigger("updatelayout");
@@ -137,6 +171,11 @@ function setGeoJSONtoMap () {
       });
 
       var marker = L.marker(latlng, {icon: icon});
+    
+                          
+      marker.on('popupopen', function(){ setFollowUserPosition(false); } );
+      //marker.on('popupclose', function(){ setFollowUserPosition(true); } );
+    
       marker.bindPopup(
         feature.properties.popupContent, {
           minHeight: feature.properties.width,
@@ -202,6 +241,54 @@ function getGeoJSON () {
   $.getJSON(geojsonbase, _cacheGeoJSON).done(setGeoJSONtoMap).fail(setGeoJSONtoMap);
 }
 
+function getMsg () {
+    $.getJSON (msgsbase, function(msgs) {
+        var lastMsgId = localStorage.getItem('lastIdMsg');
+               
+        lastMsgId = lastMsgId ? lastMsgId : -1;
+               
+        $.each(msgs, function (index, msg) {
+            if (lastMsgId > msg.msgid) return true;
+            
+            showMsg ( msg );
+                
+            return false;
+        });
+    });
+}
+
+function gotToPos (lat, lng) {
+    $.when (function () {
+        if (page !== 'mapa') $.mobile.changePage ('#mapa');
+    }).done (function () {
+        var latLng = L.latLng(lat,lng);
+             
+        setFollowUserPosition (false);
+
+        map.panTo(
+            L.latLng(lat,lng),
+            { animate: true }
+        );
+    });
+    
+}
+
+function showMsg ( msg ) {
+    navigator.notification.beep(3);
+    
+    if (msg.position) {
+        navigator.notification.alert(message, function (btnIndex) {
+            if (btnIndex == 2) {
+                goToPos (msg.position[0],msg.position[1]);
+            }
+        }, 'Achamos um local interessante', ['Ok!', 'Mostre o lugar!']);
+    } else {
+        navigator.notification.alert(msg.message, function(){}, 'Mensagem do CAUS');
+    }
+    
+    localStorage.setItem ('lastIdMsg', msg.msgid);
+}
+
 function initLocalBase () {
   localGeoJSON = localStorage.getItem('geojsonbase') ?
     JSON.parse(localStorage.getItem('geojsonbase')) :
@@ -210,8 +297,6 @@ function initLocalBase () {
 
 function setUserPin (pos) {
   var latlng = L.latLng(pos.coords.latitude,pos.coords.longitude);
-
-  console.log(JSON.stringify(pos));
 
   if (userMarker == false) {
     var icon = L.icon ({
@@ -223,23 +308,28 @@ function setUserPin (pos) {
     userMarker = L.marker(latlng, {icon: icon});
 
     userMarker.addTo(map);
+      
+    userMarker.setLatLng(latlng);
   } else {
     userMarker.setLatLng(latlng);
   }
 }
 
 function __setMapToUserLocation (pos) {
+  if (map === false) return;
   if (pos) {
     setUserPin(pos);
-    map.panTo(
-      L.latLng(pos.coords.latitude,pos.coords.longitude),
-      { animate: true }
-    );
+    if (followUser) {
+        map.panTo(
+          L.latLng(pos.coords.latitude,pos.coords.longitude),
+          { animate: true }
+        );
+    }
   }
 }
 
 function __setMapToUserLocationError (error) {
-  console.log('code: ' + error.code + '\nmessage: ' + error.message + '\n');
+  //console.log('code: ' + error.code + '\nmessage: ' + error.message + '\n');
 }
 
 function setMapToUserLocation () {
@@ -250,10 +340,76 @@ function setMapToUserLocation () {
   );
 }
 
+function initGeoLocationWatch () {
+    if (watchId === false) {
+        watchId = navigator.geolocation.watchPosition(
+            __setMapToUserLocation,
+            __setMapToUserLocationError,
+            { enableHighAccuracy: true, timeout: 1500 }
+        );
+    }
+}
+
+function stopGeoLocationWatch () {
+    if (watchId !== false) {
+        navigator.geolocation.clearWatch(watchId);
+        watchId = false;
+    }
+}
+
 function menuToggle () {
   if ( $('#menu').hasClass('opened-menu') ) {
     $('#menu').removeClass('opened-menu');
   } else {
     $('#menu').addClass('opened-menu');
   }
+}
+
+function setFollowUserPosition(status){
+    followUser = status;
+    if(status) {
+        $('.be-btn-location').fadeOut();
+    } else {
+        $('.be-btn-location').fadeIn();
+    }
+}
+
+function watchGeoTag () {
+    if (watchGeoTagsId !== false) return;
+    
+    watchGeoTagsId = setInterval (getGeoJSON, 10000);
+}
+
+function stopWatchGeoTag () {
+    if (watchGeoTagsId === false) return;
+    
+    clearInterval (watchGeoTagsId);
+    
+    watchGeoTagsId = false;
+}
+
+function watchMsg () {
+    if (watchMsgId !== false ) return;
+    
+    watchMsgId = setInterval (getMsg, 30000);
+}
+
+function stopWatchMsg() {
+    if (watchMsgId === false ) return;
+    
+    clearInterval (watchMsgId);
+    
+    watchMsgId = false;
+}
+
+function onResume () {
+    if (page == 'mapa') initGeoLocationWatch ();
+    
+    watchMsg();
+}
+
+function onPause () {
+    if (page == 'mapa') stopGeoLocationWatch ();
+    
+    stopWatchMsg ();
 }
